bee305543ae3f920bc158b486de8844055fe8d45
kamalan <chiajuichou@nycu.edu.tw>
2024-06-21 07:50:03 +0900

    Remove K1 from deepclean/couplings. Create problem: 'sub60Hz_single_pem'

--- deepclean/couplings/__init__.py
+++ deepclean/couplings/__init__.py
@@ -1,3 +1,4 @@
 from .coupling import Coupling, SubtractionProblem, subtraction_problems
 from .sub_60Hz import Sub60Hz
-#from .sub_104Hz import Sub104Hz
+from .sub_60Hz_single_pem import Sub60Hz_single_pem
+from .sub_104Hz import Sub104Hz
